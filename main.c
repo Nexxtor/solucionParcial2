5896d1f0fbf0073a5401a2272fae5252dd867c26
Nestor Aldana <nestor.aldana1@gmail.com>
2021-10-25 14:49:33 -0600

    Codigo final

--- main.c
+++ main.c
@@ -1,19 +1,49 @@
 /**
- * Crea un programa en C, que calcule la temperatura promedio de cada uno de N estudiantes
- * de la UCA, dado sus registros de todo el mes (30).
+ * Crea un programa en C, que calcule la temperatura promedio de cada uno de N
+ * estudiantes de la UCA, dado sus registros de todo el mes (30).
  * @author Néstor Aldana**/
 
 #include <stdio.h>
 
-#define NUMERO_DIAS_MES 30
+#define NUMERO_DIAS_MES 31
 
 struct estudianteTemp {
-    char carnet[9]; // 8 + '\0'
+    char carnet[9];  // 8 + '\0'
     float tempPromedio;
 };
 
 int main(int argc, char const *argv[]) {
-   
+    int n = 0;  // Cantidad de estudiantes
+
+    printf("Ingrese la cantidad de estudiantes:");
+    scanf("%d", &n);
+
+    struct estudianteTemp estudiantes[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Ingrese el carnet:");
+        scanf("%s", estudiantes[i].carnet);
+        // Solicitando la temperatura del estudiante
+        float suma = 0;
+        for (int j = 0; j < NUMERO_DIAS_MES; j++) {
+            printf("Ingrese temperatura %d:", j + 1);
+            float temp = 0;
+            scanf("%f", &temp);
+            suma += temp;
+        }
+        estudiantes[i].tempPromedio = suma / NUMERO_DIAS_MES;
+    }
+
+    // Mostrar datos
+
+    printf("Carnet\tPromedio\n");
+    float suma = 0;
+    for (int i = 0; i < n; i++) {
+        struct estudianteTemp e = estudiantes[i];
+        printf("%s\t%.1f\n", e.carnet, e.tempPromedio);
+        suma+= e.tempPromedio;
+    }
+    printf("Promedio\t%.1f\n", suma / n);
+
     return 0;
 }
-
