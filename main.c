218880ef39f196dc7eabe0d260d8fe13f6f99666
Nestor Aldana <nestor.aldana1@gmail.com>
2021-10-25 14:11:49 -0600

    Añadiendo Indicaciones

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * Crea un programa en C, que calcule la temperatura promedio de cada uno de N estudiantes
+ * de la UCA, dado sus registros de todo el mes (30).
+ * @author Néstor Aldana**/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
+
